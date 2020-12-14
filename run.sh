9525ad7a4064f3b8834f06b8f9ca9761726a4cc2
JonPoupart <75700104+JonPoupart@users.noreply.github.com>
2020-12-14 15:22:45 -0600

    commit on 'Master'

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 
 #echo "sv_rcon_password $(pwgen -s 12 1)" >> /${GAME_TYPE}.cfg
 #cat /${GAME_TYPE}.cfg
-#exec /usr/games/teeworlds-server -f DanS1VM1.cfg
-exec teeworlds-server -f DanS1VM1.cfg
+exec /usr/games/teeworlds-server -f DanS1VM1.cfg
+#exec teeworlds-server -f DanS1VM1.cfg
